42de861bbe06fd0dcfe63dfc3611669163407ab1
Krzysztof Szabelski <kszabelski@gmail.com>
2015-08-04 14:51:53 +0200

    Refactoring towards clean start point.

---
+++ GameBoardState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnakeWars.SampleBot
+{
+    internal class GameBoardState
+    {
+        private readonly GameStateDTO _gameState;
+
+        public GameBoardState(GameStateDTO gameState)
+        {
+            _gameState = gameState;
+        }
+
+        public PointDTO GetSnakeNewHeadPosition(string snakeId, MoveDirection move)
+        {
+            var snake = GetSnake(snakeId);
+            var newHead = move.GetSnakeNewHead(snake, _gameState.BoardSize);
+            return newHead;
+        }
+        
+        public HashSet<PointDTO> GetOccupiedCells()
+        {
+            return new HashSet<PointDTO>(_gameState.Walls.Concat(_gameState.Snakes.SelectMany(snake => snake.Cells)));
+        }
+
+        public SnakeDTO GetSnake(string snakeId)
+        {
+            return _gameState.Snakes.First(s => s.Id == snakeId);
+        }
+    }
+}
